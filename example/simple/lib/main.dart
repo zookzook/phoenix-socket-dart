4ca237bcf7f841e9dc3e88838e196b48e2453ef1
zookzook <zookzook@unitybox.de>
2023-12-01 14:06:08 +0100

    added support for binary messages

--- example/simple/lib/main.dart
+++ example/simple/lib/main.dart
@@ -2,18 +2,21 @@ import 'dart:async';
 
 import 'package:phoenix_socket/phoenix_socket.dart';
 import 'package:uuid/uuid.dart';
-import 'package:logging/logging.dart';
 
 void main() async {
-  Logger.root.level = Level.FINEST;
-  Logger.root.onRecord.listen((record) {
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
-  final socket = PhoenixSocket('ws://localhost:4001/socket/websocket?token=540f6ea8-c980-46dd-a040-c9a62958487a&app_version=10.0.0&vsn=2.0.0&binary=true&platform=macos');
-  await socket.connect();
-  var channel = socket.addChannel(topic: 'collaborators:540f6ea8-c980-46dd-a040-c9a62958487a');
-  await channel.join().future;
-  await for (var message in channel.messages) {
-    print("received ${message.event} with payload ${message.payload}");
+  final socket1 = PhoenixSocket('ws://localhost:4001/socket/websocket');
+  await socket1.connect();
+  var channel1 = socket1.addChannel(topic: 'channel3');
+  await channel1.join().future;
+  var uuid = Uuid().v4();
+
+  channel1.push('ping', {'from': uuid});
+
+  await for (var message in channel1.messages) {
+    if (message.event != 'pong' || message.payload?.toJson()['from'] == uuid) continue;
+    print("received ${message.event} from ${message.payload!.toJson()['from']}");
+    Timer(const Duration(seconds: 1), () {
+      channel1.push('ping', {'from': uuid});
+    });
   }
 }
